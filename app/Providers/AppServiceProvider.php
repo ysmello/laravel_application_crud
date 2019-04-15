fb5db7eb87bcf63dc78ca6efe5935b1e43f04f70
Yuri de Mello <yuridemeello@gmail.com>
2019-04-15 18:32:15 -0300

    Traducao de dados falsos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(\Fake\Generator::class, function() {
-            return \Fake\Generator::create('pt_BR');
+        $this->app->singleton(\Faker\Generator::class, function() {
+            return \Faker\Factory::create('pt_BR');
         });
     }
 
